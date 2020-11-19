2bdc4ff47e376d984241b472061e0687077ee684
bastianarf <bastianarf@gmail.com>
2020-11-19 23:24:45 +0700

    second commit

---
+++ app/Views/product_view.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>List Produk</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+    <br>
+    <h3>List Produk</h3>
+    <br>
+    <div class="text-right">
+    <button type="button" class="btn btn-success mb-2" data-toggle="modal" data-target="#addModal">Tambah Produk Baru</button>
+    </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Nama Produk</th>
+                    <th>Harga</th>
+                    <th>Kategori</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach($product as $row):?>
+                <tr>
+                    <td><?= $row->product_name;?></td>
+                    <td><?= $row->product_price;?></td>
+                    <td><?= $row->category_name;?></td>
+                    <td>
+                        <a href="#" class="btn btn-info btn-sm btn-edit" data-id="<?= $row->product_id;?>" data-name="<?= $row->product_name;?>" data-price="<?= $row->product_price;?>" data-category_id="<?= $row->product_category_id;?>">Edit</a>
+                        <a href="#" class="btn btn-danger btn-sm btn-delete" data-id="<?= $row->product_id;?>">Hapus</a>
+                    </td>
+                </tr>
+            <?php endforeach;?>
+            </tbody>
+        </table>
+
+    </div>
+    
+    <!-- Modal Add Product-->
+    <form action="/product/save" method="post">
+        <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Produk Baru</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            
+                <div class="form-group">
+                    <label>Nama Produk</label>
+                    <input type="text" class="form-control" name="product_name" placeholder="Product Name">
+                </div>
+                
+                <div class="form-group">
+                    <label>Harga</label>
+                    <input type="text" class="form-control" name="product_price" placeholder="Product Price">
+                </div>
+
+                <div class="form-group">
+                    <label>Kategori</label>
+                    <select name="product_category" class="form-control">
+                        <option value="">- Pilih -</option>
+                        <?php foreach($category as $row):?>
+                        <option value="<?= $row->category_id;?>"><?= $row->category_name;?></option>
+                        <?php endforeach;?>
+                    </select>
+                </div>
+            
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </form>
+    <!-- End Modal Add Product-->
+
+    <!-- Modal Edit Product-->
+    <form action="/Product/update" method="post">
+        <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Produk</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            
+                <div class="form-group">
+                    <label>Nama Produk</label>
+                    <input type="text" class="form-control product_name" name="product_name" placeholder="Product Name">
+                </div>
+                
+                <div class="form-group">
+                    <label>Harga</label>
+                    <input type="text" class="form-control product_price" name="product_price" placeholder="Product Price">
+                </div>
+
+                <div class="form-group">
+                    <label>Kategori</label>
+                    <select name="product_category" class="form-control product_category">
+                        <option value="">-Pilih-</option>
+                        <?php foreach($category as $row):?>
+                        <option value="<?= $row->category_id;?>"><?= $row->category_name;?></option>
+                        <?php endforeach;?>
+                    </select>
+                </div>
+            
+            </div>
+            <div class="modal-footer">
+                <input type="hidden" name="product_id" class="product_id">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                <button type="submit" class="btn btn-primary">Update</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </form>
+    <!-- End Modal Edit Product-->
+
+    <!-- Modal Delete Product-->
+    <form action="/product/delete" method="post">
+        <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Hapus Produk</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            
+               <h4>Apa kamu yakin untuk menghapus produk ini?</h4>
+            
+            </div>
+            <div class="modal-footer">
+                <input type="hidden" name="product_id" class="productID">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tidak</button>
+                <button type="submit" class="btn btn-primary">Ya</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </form>
+    <!-- End Modal Delete Product-->
+
+
+<script src="/js/jquery.min.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
+<script>
+    $(document).ready(function(){
+
+        // get Edit Product
+        $('.btn-edit').on('click',function(){
+            // get data from button edit
+            const id = $(this).data('id');
+            const name = $(this).data('name');
+            const price = $(this).data('price');
+            const category = $(this).data('category_id');
+            // Set data to Form Edit
+            $('.product_id').val(id);
+            $('.product_name').val(name);
+            $('.product_price').val(price);
+            $('.product_category').val(category).trigger('change');
+            // Call Modal Edit
+            $('#editModal').modal('show');
+        });
+
+        // get Delete Product
+        $('.btn-delete').on('click',function(){
+            // get data from button edit
+            const id = $(this).data('id');
+            // Set data to Form Edit
+            $('.productID').val(id);
+            // Call Modal Edit
+            $('#deleteModal').modal('show');
+        });
+        
+    });
+</script>
+</body>
+</html>
